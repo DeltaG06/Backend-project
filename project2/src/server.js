a9fd04ccd0e3ba265ca46740513353be710372df
Gaurav Parker <gauravparker955@gmail.com>
2025-03-03 00:21:15 +0530

    added path.join

--- project2/src/server.js
+++ project2/src/server.js
@@ -1,11 +1,21 @@
 // Import express from express
 import express from 'express';
+import path,{ dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 6439
 
-console.log('Hello World!');
+// Get ther file path from the URL of current module
+const __filename = fileURLToPath(import.meta.url)
+//Get the directory name from the file path
+const __dirname = dirname(__filename)
+//serving up hTML filefrom /public directory
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname,'public','index.html'));
+
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
