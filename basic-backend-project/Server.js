f38bc013dec377e8c283f5f9252a9e1b90afebf9
Gaurav Parker <gauravparker955@gmail.com>
2025-03-02 20:17:04 +0530

    delete request

--- basic-backend-project/Server.js
+++ basic-backend-project/Server.js
@@ -23,6 +23,7 @@ app.get('/', (req, res) => {
        font-family: Arial, sans-serif; 
        text-align: center;">
       <p>${JSON.stringify(data)}</p>
+      <a href="/dashboard">Dashboard</a>
       </body>
     `);
   
@@ -32,12 +33,32 @@ app.get('/', (req, res) => {
 app.get('/dashboard', (req, res) => {
    console.log('Reached dashboard');
     
-   res.send('<h>Welcome to the dashboard!</h>')  
+   res.send(`<body 
+              style="background-color:lightblue; 
+              color: blue; font-family: Arial, sans-serif;
+               text-align: center;">
+               <h1>
+               Dashboard
+               </h1>
+               <p>${JSON.stringify(data)}</p>
+               <a href="/api/data">
+               DATA</a>
+               </body>`);  
 
 }) 
 app.get('/api/data', (req, res) => {
-  res.send(data); 
+
+  res.send(`<body style="background-color:lightblue;
+               color: blue;
+                font-family: Arial, sans-serif; 
+                text-align: center;">
+                <p>${JSON.stringify(data)}</p>
+                <h1>DATA</h1>
+                <a href="/">
+                home</a>
+                </body>`); 
   console.log('Reached data page');
+ 
 })  
 
 app.post('/api/data', (req, res) => {
@@ -48,10 +69,18 @@ app.post('/api/data', (req, res) => {
   console.log(newEntry);
   data.push(newEntry);
   
-
-  
 }
 )  
+app.delete('/api/data', (req, res) => {
+  //someone wants to delete a user
+  //user clicks the button  and browser sends a request to the server to complete the action
+  
+  data.pop();
+  console.log("Deleted endpoint ");
+  
+  
+}
+)
 
 app.listen(Port, () => console.log(`Server is running on : ${Port}`));
 
