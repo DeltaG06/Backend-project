71d3f37ab0450dafe68decbbae66a610f2282e11
Gaurav Parker <gauravparker955@gmail.com>
2025-03-03 13:09:32 +0530

    added todo routes

---
+++ project2/src/routes/todoRoute.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import db from '../db.js'
+
+const router = express.Router()
+
+//get all todos for logged in user
+
+router.get('/',(req,res) => {
+
+})
+//create a new todo
+
+router.get('/',(req,res) => {
+
+})
+
+//update a todo (modify using put)
+
+router.put('/:id', (req,res) => {
+
+})
+
+//delete a todo
+router.delete('/:id',(req,res) => { 
+
+})
+
+export default router
--- project2/src/server.js
+++ project2/src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path,{ dirname } from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoute.js'
 
 const app = express();
 const PORT = process.env.PORT || 6439
@@ -26,6 +27,7 @@ app.get('/', (req, res) => {
 
 //routes 
 app.use('/auth',authRoutes)
+app.use('/todos',authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
